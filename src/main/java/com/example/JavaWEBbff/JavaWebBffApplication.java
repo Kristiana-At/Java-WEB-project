361a48fd718bc217b612fcc2e70d5e13397dbe09
Kristiana-At <77620189+Kristiana-At@users.noreply.github.com>
2023-04-11 17:10:47 +0300

    add bff project

---
+++ src/main/java/com/example/JavaWEBbff/JavaWebBffApplication.java
@@ -0,0 +1,13 @@
+package com.example.JavaWEBbff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWebBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaWebBffApplication.class, args);
+	}
+
+}
